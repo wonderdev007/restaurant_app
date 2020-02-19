d2748bf8fbf04044f667e3329c9bf62bfe2ce6ca
wonderdev007 <rogerwithmail@gmail.com>
2020-02-19 12:29:57 +0000

    building single menu page

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,10 @@ Route::get('/menu', function () {
     return view('menu.index');
 });
 
+Route::get('/menu/{slug}', function () {
+    return view('menu.single-menu');
+});
+
 Route::get('/about', function() {
     return view('pages.about');
 });
