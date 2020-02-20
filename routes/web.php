ce6dcee27b0935d404d44ae13bd524233cf46b8a
wonderdev007 <rogerwithmail@gmail.com>
2020-02-20 20:44:17 +0000

    creating controllers for static pages & admin sections

--- routes/web.php
+++ routes/web.php
@@ -11,58 +11,29 @@
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+// Static Pages
+Route::get('/', 'StaticPagesController@home');
+Route::get('/menu', 'StaticPagesController@menu');
+Route::get('/menu/{slug}', 'StaticPagesController@singleMenu');
+Route::get('/about', 'StaticPagesController@about');
+Route::get('/contact', 'StaticPagesController@contact');
+Route::get('/waitlist', 'StaticPagesController@waitlist');
+Route::get('/offers', 'StaticPagesController@offers');
 
-Route::get('/admin', function () {
-    return view('admin/dashboard');
-});
-Route::get('/admin/food-categories', function () {
-    return view('admin/food-categories/all');
-});
+// Admin Dashboard
+Route::get('/admin', 'admin\AdminController@dashboard');
 
-Route::get('/admin/food-categories/create', function () {
-    return view('admin/food-categories/create');
-});
+// Admin Food Categories
+Route::get('/admin/food-categories', 'admin\FoodCategoriesController@index');
+Route::get('/admin/food-categories/create', 'admin\FoodCategoriesController@create');
+Route::get('/admin/food-categories/{id}/edit', 'admin\FoodCategoriesController@edit');
 
-Route::get('/admin/food-categories/{id}/edit', function () {
-    return view('admin/food-categories/edit');
-});
-
-
+// Admin Authentication
 Route::get('/admin/register', function () {
     return view('admin/register');
 });
-
 Route::get('/admin/login', function () {
     return view('admin/login');
 });
 
-Route::get('/menu', function () {
-    return view('menu/index');
-});
 
-Route::get('/menu/{slug}', function () {
-    return view('menu/single-menu');
-});
-
-Route::get('/about', function() {
-    return view('pages/about');
-});
-
-Route::get('/contact', function() {
-    return view('pages/contact');
-});
-
-Route::get('/waitlist', function() {
-    return view('pages/waitlist');
-});
-
-Route::get('/giftcards', function() {
-    return view('pages/giftcards');
-});
-
-Route::get('/offers', function() {
-    return view('pages/offers');
-});
