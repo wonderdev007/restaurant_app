a2d26d3ff9be44b8d4d091da522824f7d293d47f
wonderdev007 <rogerwithmail@gmail.com>
2020-02-25 13:25:12 +0000

    creating roles, migrations and model relationships

---
+++ database/migrations/2020_02_25_130312_create_role_user_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRoleUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('role_user', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('role_id')->unsigned();
+            $table->bigInteger('user_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('role_id')->references('id')->on('roles')->onCascade('delete');
+            $table->foreign('user_id')->references('id')->on('users')->onCascade('delete');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('role_user');
+    }
+}
