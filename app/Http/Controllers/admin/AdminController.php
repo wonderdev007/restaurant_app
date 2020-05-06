53bfbc874bfd00d8268f0e500d2c97e2f98b3a32
wonderdev007 <rogerwithmail@gmail.com>
2020-05-06 12:25:34 +0100

    displaying total revenue, customers, reservations and employees in admin dashboard

--- app/Http/Controllers/admin/AdminController.php
+++ app/Http/Controllers/admin/AdminController.php
@@ -4,13 +4,48 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\User;
+use App\Member;
+use App\Reservation;
+use DB;
 
 class AdminController extends Controller
 {
     public function __construct() {
         $this->middleware('auth');
     }
-    public function dashboard() {
-        return view('admin/dashboard');
+    public function dailyRevenueLast30() {
+        $estimated_income_last_30 = DB::select(DB::raw('
+            SELECT (sum(guests_total) * 27) as total
+            FROM reservations
+            WHERE created_at BETWEEN CURDATE() - INTERVAL 30 DAY AND CURDATE()
+        '));
+
+        $total_customers_last_30 = DB::select(DB::raw('
+            SELECT sum(guests_total) as total
+            FROM reservations
+            WHERE created_at BETWEEN CURDATE() - INTERVAL 30 DAY AND CURDATE()
+        '));
+
+        $total_reservations_last_30 = DB::select(DB::raw('
+            SELECT count(*) as total
+            FROM reservations
+            WHERE created_at BETWEEN CURDATE() - INTERVAL 30 DAY AND CURDATE()
+        '));
+
+        $total_employees = DB::select(DB::raw('
+            SELECT count(*) as total
+            FROM users
+            INNER JOIN role_user ON role_user.user_id = users.id
+            INNER JOIN roles ON roles.id = role_user.role_id
+            WHERE role_id = 2
+        '));
+
+        return view('admin/dashboard', [
+            'estimated_income_last_30' =>  $estimated_income_last_30[0]->total,
+            'total_customers_last_30' => $total_customers_last_30[0]->total,
+            'total_reservations_last_30' => $total_reservations_last_30[0]->total,
+            'total_employees' => $total_employees[0]->total,
+        ]);
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -36,9 +36,9 @@
                 <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
                   <div class="card">
                     <div class="card-body">
-                      <h5 class="text-muted">Total Revenue</h5>
+                      <h5 class="text-muted">Estimated Revenue</h5>
                       <div class="metric-value d-inline-block">
-                        <h1 class="mb-1">$12099</h1>
+                        <h1 class="mb-1">&pound;{{$estimated_income_last_30}}</h1>
                       </div>
                       <div class="metric-label d-inline-block float-right text-success font-weight-bold">
                         <span>
@@ -46,15 +46,15 @@
                         </span><span>5.86%</span>
                       </div>
                     </div>
-                    <div id="sparkline-revenue"></div>
+                    {{-- <div id="sparkline-revenue"></div> --}}
                   </div>
                 </div>
                 <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
                   <div class="card">
                     <div class="card-body">
-                      <h5 class="text-muted">Affiliate Revenue</h5>
+                      <h5 class="text-muted">Total Customers</h5>
                       <div class="metric-value d-inline-block">
-                        <h1 class="mb-1">$12099</h1>
+                        <h1 class="mb-1">&pound;{{$total_customers_last_30}}</h1>
                       </div>
                       <div class="metric-label d-inline-block float-right text-success font-weight-bold">
                         <span>
@@ -62,35 +62,35 @@
                         </span><span>5.86%</span>
                       </div>
                     </div>
-                    <div id="sparkline-revenue2"></div>
+                    {{-- <div id="sparkline-revenue2"></div> --}}
                   </div>
                 </div>
                 <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
                   <div class="card">
                     <div class="card-body">
-                      <h5 class="text-muted">Refunds</h5>
+                      <h5 class="text-muted">Total Reservations</h5>
                       <div class="metric-value d-inline-block">
-                        <h1 class="mb-1">0.00</h1>
+                        <h1 class="mb-1">{{$total_reservations_last_30}}</h1>
                       </div>
                       <div class="metric-label d-inline-block float-right text-primary font-weight-bold">
                         <span>N/A</span>
                       </div>
                     </div>
-                    <div id="sparkline-revenue3"></div>
+                    {{-- <div id="sparkline-revenue3"></div> --}}
                   </div>
                 </div>
                 <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
                   <div class="card">
                     <div class="card-body">
-                      <h5 class="text-muted">Avg. Revenue Per User</h5>
+                      <h5 class="text-muted">Total Employees</h5>
                       <div class="metric-value d-inline-block">
-                        <h1 class="mb-1">$28000</h1>
+                        <h1 class="mb-1">{{$total_employees}}</h1>
                       </div>
                       <div class="metric-label d-inline-block float-right text-secondary font-weight-bold">
                         <span>-2.00%</span>
                       </div>
                     </div>
-                    <div id="sparkline-revenue4"></div>
+                    {{-- <div id="sparkline-revenue4"></div> --}}
                   </div>
                 </div>
               </div>
